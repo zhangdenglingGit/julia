8e0791aba6a047a9c93c2bd1047ceead5d8de26c
Katharine Hyatt <khyatt@flatironinstitute.org>
2019-03-29 17:42:47 -0400

    doc AbstractWorkerPool

--- stdlib/Distributed/src/workerpool.jl
+++ stdlib/Distributed/src/workerpool.jl
@@ -1,5 +1,20 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+"""
+    AbstractWorkerPool
+
+Supertype for worker pools such as [`WorkerPool`](@ref) and [`CachingPool`](@ref).
+An `AbstractWorkerPool` should implement:
+  - [`push!`](@ref) - add a new worker to the overall pool (available + busy)
+  - [`put!`](@ref) - put back a worker to the available pool
+  - [`take!`](@ref) - take a worker from the available pool (to be used for remote function execution)
+  - [`length`](@ref) - number of workers available in the overall pool
+  - [`isready`](@ref) - return false if a `take!` on the pool would block, else true
+
+The default implementations of the above (on a `AbstractWorkerPool`) require fields
+    channel::Channel{Int}
+    workers::Set{Int}
+"""
 abstract type AbstractWorkerPool end
 
 # An AbstractWorkerPool should implement
